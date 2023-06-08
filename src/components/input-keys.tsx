62cdb415f6c2ef747efb0ce7bd0fe2018d15f831
Jordane Gengo <jordane@hive.fi>
2023-06-08 14:15:32 +0300

    chore: remove useless import

--- src/components/input-keys.tsx
+++ src/components/input-keys.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import * as React from 'react';
-// import { useState } from 'react';
+
 import { Input } from '@/components/ui/input';
 import { Icons } from '@/components/icons';
 
