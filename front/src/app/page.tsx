9720ed3141ebc0beee1b978b35a9f0719d2f9bb4
Jordane Gengo <jordane@hive.fi>
2023-06-15 13:01:10 +0300

    chore: restruct project dir

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -30,22 +30,22 @@ export default function Home() {
       </section>
       <section className="mt-20 flex items-center justify-center">
         <Image
-          src="https://picsum.photos/600/400"
-          width={600}
-          height={400}
+          src="https://picsum.photos/1024/600"
+          width={1024}
+          height={600}
           alt="placeholder"
         />
       </section>
       <section className="mt-20 space-y-16 md:grid md:grid-cols-2 md:gap-x-6 md:gap-y-16 md:space-y-0 lg:gap-x-10">
         <article>
           <h2 className="text-lg font-semibold leading-7">
-            What is lorem ipsum?
+            Why would I need this?
           </h2>
           <p>
-            Lorem ipsum is Lorem ipsum dolor sit, amet consectetur adipisicing
-            elit. Ex, quisquam repudiandae? Deleniti repellat porro ad sapiente
-            eum quis sint laborum eveniet et excepturi accusamus dolor
-            molestiae, mollitia voluptatum culpa? Aperiam!
+            42 API apps are linked to an individual account, it can become
+            challenging to manage and keep track of them. API Keychain would
+            like to help team to overwatch all their API apps and their rotation
+            deadlines to avoid disruption of their services.
           </p>
         </article>
         <article>
@@ -56,7 +56,11 @@ export default function Home() {
             Lorem ipsum is Lorem ipsum dolor sit, amet consectetur adipisicing
             elit. Ex, quisquam repudiandae? Deleniti repellat porro ad sapiente
             eum quis sint laborum eveniet et excepturi accusamus dolor
-            molestiae, mollitia voluptatum culpa? Aperiam!
+            molestiae, mollitia voluptatum culpa? Aperiam! Lorem ipsum is Lorem
+            ipsum dolor sit, amet consectetur adipisicing elit. Ex, quisquam
+            repudiandae? Deleniti repellat porro ad sapiente eum quis sint
+            laborum eveniet et excepturi accusamus dolor molestiae, mollitia
+            voluptatum culpa? Aperiam!
           </p>
         </article>
         <article>
