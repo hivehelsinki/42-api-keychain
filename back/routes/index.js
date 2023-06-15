9a7bac349503c0be1acc289e9107dcff08bdfadf
Jordane Gengo <jordane@hive.fi>
2023-06-15 13:59:58 +0300

    feat: starting to work on the backend

--- back/routes/index.js
+++ back/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send({name: "api-keychain (backend)", version: "0.0.1"});
 });
 
 module.exports = router;
