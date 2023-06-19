cd3dbe6529d6cef2bf6d45797786c2ad1714ed2c
Titus <jgengo@student.42.fr>
2023-06-19 21:18:52 +0300

    feat: moving forward with the navbar info

--- front/src/components/auth/login-button.tsx
+++ front/src/components/auth/login-button.tsx
@@ -1,8 +1,14 @@
 'use client';
-import { signIn } from 'next-auth/react';
+
+import { signIn, signOut } from 'next-auth/react';
+import { getCurrentUser } from '@/lib/session';
 import { Button } from '@/components/ui/button';
 
-const LoginButton = () => {
+const LoginButton = async () => {
+  const user = await getCurrentUser();
+  if (user) {
+    return <>Hello {user.login}</>;
+  }
   return (
     <Button onClick={() => signIn('42-school', { callbackUrl: '/' })}>
       Sign in
