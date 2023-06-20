85b5400b471f9ee58143cad7e73e045c31eb11f4
Jordane Gengo <jordane@hive.fi>
2023-06-20 16:23:09 +0300

    feat: added table to display keys (key is dummy key dw)

--- front/src/app/(admin)/dashboard/page.tsx
+++ front/src/app/(admin)/dashboard/page.tsx
@@ -4,6 +4,13 @@ import Link from 'next/link';
 import useSWR from 'swr';
 
 import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 
 export default function Dashboard() {
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
@@ -24,6 +31,54 @@ export default function Dashboard() {
           <Button size="sm">Add new one</Button>
         </Link>
       </div>
+
+      <section className="mt-10">
+        <Card className="col-span-3">
+          <CardHeader>
+            <CardTitle>Keychain</CardTitle>
+            <CardDescription>You have 10 keys.</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="space-y-8">
+              <div className="flex items-center">
+                <div className="space-y-1">
+                  <p className="text-sm font-medium leading-none">
+                    HIVE - API-KEYCHAIN
+                  </p>
+                  <p className="hidden text-sm text-muted-foreground md:block">
+                    u-s4t2af-a9ad7a9462d4ca4767c5c464ccae0e4527d7d311d77819c380339b1f33b804ec
+                  </p>
+                </div>
+                <div className="ml-auto font-medium">13/06/2024</div>
+              </div>
+
+              <div className="flex items-center">
+                <div className="space-y-1">
+                  <p className="text-sm font-medium leading-none">
+                    HIVE - API-KEYCHAIN
+                  </p>
+                  <p className="hidden text-sm text-muted-foreground md:block">
+                    u-s4t2af-a9ad7a9462d4ca4767c5c464ccae0e4527d7d311d77819c380339b1f33b804ec
+                  </p>
+                </div>
+                <div className="ml-auto font-medium">13/06/2024</div>
+              </div>
+
+              <div className="flex items-center">
+                <div className="space-y-1">
+                  <p className="text-sm font-medium leading-none">
+                    HIVE - API-KEYCHAIN
+                  </p>
+                  <p className="hidden text-sm text-muted-foreground md:block">
+                    u-s4t2af-a9ad7a9462d4ca4767c5c464ccae0e4527d7d311d77819c380339b1f33b804ec
+                  </p>
+                </div>
+                <div className="ml-auto font-medium">13/06/2024</div>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+      </section>
 
       {/* <ul className='bg-red-300 mt-5'>
         hey
