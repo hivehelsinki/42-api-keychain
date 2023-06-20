7be7650c96e521d828f63eb78930783244c5abfa
Jordane Gengo <jordane@hive.fi>
2023-06-20 09:34:47 +0300

    chore: added a comment in [...nextauth] and eslint avatar

--- front/src/pages/api/auth/[...nextauth].ts
+++ front/src/pages/api/auth/[...nextauth].ts
@@ -2,4 +2,7 @@ import NextAuth from 'next-auth';
 
 import { authOptions } from '@/lib/auth';
 
+// could add a check here to make sure all env vars are set
+// if (!process.env.SECRET) throw new Error('SECRET is not set');
+
 export default NextAuth(authOptions);
