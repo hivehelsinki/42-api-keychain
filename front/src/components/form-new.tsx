4e6658f2a96ece7927334625a5e0e3d423b00058
Jordane Gengo <jordane@hive.fi>
2023-06-28 15:22:45 +0300

    feat: prep for the add key form

--- front/src/components/form-new.tsx
+++ front/src/components/form-new.tsx
@@ -1,16 +1,98 @@
 'use client';
 
-import { InputKeys } from '@/components/input-keys';
+// import { InputKeys } from '@/components/input-keys';
+import * as React from 'react';
 import { Button } from '@/components/ui/button';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import { Icons } from '@/components/icons';
 
 const FormNew = () => {
+  const [uid, setUid] = React.useState<string>('');
+  const [secret, setSecret] = React.useState<string>('');
+  const [isValid, setIsValid] = React.useState<boolean | null>(null);
+  const [data, setData] = React.useState({});
+
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    setInput: React.Dispatch<React.SetStateAction<string>>
+  ) => {
+    setInput(event.target.value);
+  };
+
+  React.useEffect(() => {
+    if (uid.length >= 64 && secret.length >= 64) {
+      const fetchData = async () => {
+        try {
+          const res = await fetch('/api/keys/check', {
+            method: 'POST',
+            body: JSON.stringify({ client_id: uid, client_secret: secret }),
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          });
+          const data = await res.json();
+          setData(data);
+          console.log(data);
+          setIsValid(true);
+        } catch (error) {
+          console.error('Error checking inputs:', error);
+          setIsValid(false);
+        }
+      };
+      fetchData();
+    } else {
+      setIsValid(null);
+    }
+  }, [uid, secret]);
+
   return (
     <section className="flex max-w-[700px] flex-col gap-5">
       <h1 className="text-2xl font-bold leading-tight tracking-tighter md:text-3xl lg:leading-[1.1]">
         Add a new application
       </h1>
       <div className="mt-6">
-        <InputKeys />
+        <div className="flex w-full flex-col gap-2">
+          <Label htmlFor="uid">Client ID</Label>
+          <Input
+            id="uid"
+            value={uid}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              handleInputChange(event, setUid)
+            }
+          />
+
+          <Label className="mt-4" htmlFor="secret">
+            Client Secret
+          </Label>
+          <Input
+            id="secret"
+            value={secret}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              handleInputChange(event, setSecret)
+            }
+          />
+
+          {isValid !== null && (
+            <div className="mt-4">
+              {isValid ? (
+                <div className="flex items-center gap-2 bg-green-100 py-2 pl-2 text-sm dark:bg-green-700">
+                  <Icons.check className="h-4 w-4 text-green-600 dark:text-gray-200" />
+                  <p className="dark:text-gray-200">
+                    Application found under the name <b>{data?.appName}</b>
+                  </p>
+                </div>
+              ) : (
+                <div className="flex items-center gap-2 bg-red-100 py-2 pl-2 text-sm dark:bg-destructive dark:bg-red-500">
+                  <Icons.close className="h-4 w-4 text-red-600/80 dark:text-gray-200" />
+                  <p className="dark:text-gray-200">
+                    Application not found or not valid
+                  </p>
+                </div>
+              )}
+            </div>
+          )}
+        </div>
       </div>
       <Button disabled>Add</Button>
     </section>
