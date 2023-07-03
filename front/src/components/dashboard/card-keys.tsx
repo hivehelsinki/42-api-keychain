9ff71f2426bdc2cc23d9cd52ee1fe07077a5019f
Titus <jgengo@student.42.fr>
2023-07-03 16:06:59 +0300

    feat: add hover on dashboard cards

--- front/src/components/dashboard/card-keys.tsx
+++ front/src/components/dashboard/card-keys.tsx
@@ -12,7 +12,7 @@ import { Ping } from '@/components/ping';
 import { Button } from '@/components/ui/button';
 
 const RenderElem = ({ datum, ...props }) => (
-  <Card className="group" {...props}>
+  <Card className="group hover:bg-gray-100" {...props}>
     <CardHeader className="relative">
       <Ping className="absolute -right-2 -top-2" />
       <CardTitle>{datum.name}</CardTitle>
