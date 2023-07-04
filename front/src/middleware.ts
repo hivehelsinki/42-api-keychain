277dcb027b80e0349617453fe686bf5965b821eb
Jordane Gengo <jordane@hive.fi>
2023-07-04 09:36:12 +0300

    feat: add middleware to prevent reaching /dashboard if not logged in

--- front/src/middleware.ts
+++ front/src/middleware.ts
@@ -1,12 +1,18 @@
 import { getToken } from 'next-auth/jwt';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { getCurrentUser } from '@/lib/session';
 
 export async function middleware(req: NextRequest) {
   const token = await getToken({ req });
-  console.log(token);
   const isAuth = !!token;
-  console.log(isAuth);
+  // const isHome = req.nextUrl.pathname.match(/^\/$/);
+
+  // if (isHome) {
+  //   if (isAuth) {
+  //     return NextResponse.redirect(new URL('/dashboard', req.url));
+  //   }
+  // }
 
   if (!isAuth) {
     return NextResponse.redirect(new URL('/', req.url));
@@ -14,5 +20,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/dashboard'],
+  matcher: ['/dashboard', '/new'],
 };
