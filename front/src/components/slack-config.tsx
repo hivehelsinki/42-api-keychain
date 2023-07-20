01a93d475a39d5fcb1ba1af5b48631f630b51cb8
Jordane Gengo <jordane@hive.fi>
2023-07-20 14:08:06 +0300

    feat: working on display and updating the settings

--- front/src/components/slack-config.tsx
+++ front/src/components/slack-config.tsx
@@ -1,25 +1,61 @@
-import { FC } from 'react';
+'use client';
+
+import { FC, useEffect, useState } from 'react';
 import { Label } from '@/components/ui/label';
 import { Switch } from '@/components/ui/switch';
 import { Input } from './ui/input';
 
 interface slackConfigProps {}
 
-async function getSlackConfig() {}
-
 const SlackConfig: FC<slackConfigProps> = ({}) => {
+  const [slackEnabled, setSlackEnabled] = useState<boolean>(false);
+
+  useEffect(() => {
+    fetchSettings();
+  }, []);
+
+  const fetchSettings = async () => {
+    try {
+      const res = await fetch('http://localhost:5001/settings');
+      const { slack_enabled } = await res.json();
+      setSlackEnabled(slack_enabled === 'true' ? true : false);
+    } catch (error) {
+      console.error('error fetching settings', error);
+    }
+  };
+
+  const handleChange = async () => {
+    try {
+      const res = await fetch('http://localhost:5001/settings/slack_enabled', {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ value: !slackEnabled }),
+      });
+
+      setSlackEnabled(!slackEnabled);
+    } catch (error) {
+      console.error('error updating settings', error);
+    }
+  };
+
   return (
     <div className="flex max-w-[700px] flex-col gap-2">
       <div className="flex flex-col">
         <Label htmlFor="slack-enabled" className="font-bold uppercase">
           Slack notification
         </Label>
-        <p className="my-2 text-muted-foreground ">
+        <p className="my-2 leading-7 text-muted-foreground">
           Receive notification on Slack when an app is about to be expired
         </p>
         <div className="flex items-center gap-2">
-          <Switch id="slack-enabled" />
-          <p className="font-bold">Disabled</p>
+          <Switch
+            id="slack-enabled"
+            checked={slackEnabled}
+            onClick={handleChange}
+          />
+          <p className="font-bold">{slackEnabled ? 'Enabled' : 'Disabled'}</p>
         </div>
       </div>
 
@@ -27,7 +63,7 @@ const SlackConfig: FC<slackConfigProps> = ({}) => {
         <Label className="font-bold uppercase" htmlFor="slack-webhook-url">
           Slack Webhook
         </Label>
-        <p className="my-2 text-muted-foreground">
+        <p className="my-2 leading-7 text-muted-foreground">
           Your webhook url to send notification to slack
         </p>
         <Input
