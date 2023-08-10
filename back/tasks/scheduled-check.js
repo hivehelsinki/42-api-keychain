8bd08ba37c1cbbd4c1ae7de27bc9ffbde1721892
Jordane Gengo <jordane@hive.fi>
2023-08-10 14:00:58 +0300

    feat(back): finalizing dockerizing the backend

--- back/tasks/scheduled-check.js
+++ back/tasks/scheduled-check.js
@@ -1,13 +1,9 @@
 const cron = require("node-cron");
 const { CheckKeys } = require("./check-keys");
 
-async function runApiKeyValidationTask() {
-  console.log("running key validity check...");
-  CheckKeys();
-}
-
 module.exports = {
   schedule: () => {
-    cron.schedule("0 10 */1 * *", runApiKeyValidationTask); // every day at 10:00
+    CheckKeys();
+    cron.schedule("0 10 */1 * *", CheckKeys); // every day at 10:00
   },
 };
