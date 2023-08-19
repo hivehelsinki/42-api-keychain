de251cce742163430848ddd3d083a3ba41d2fa65
Titus <jgengo@student.42.fr>
2023-08-19 12:26:50 +0300

    fix(back): error handling, separation of concerns, connection management

--- back/tasks/populate-database.js
+++ back/tasks/populate-database.js
@@ -10,15 +10,24 @@ async function populateDatabase() {
 
   try {
     for (const { key, value } of defaultSettings) {
-      await prisma.Setting.upsert({
-        create: { settingKey: key, settingValue: value },
-        where: { settingKey: key },
-        update: {},
-      });
+      await upsertSetting(prisma, key, value);
     }
   } catch (error) {
+    console.error(`Error populating database: ${error.message}`);
     throw error;
+  } finally {
+    await prisma.$disconnect();
   }
 }
 
-populateDatabase();
+async function upsertSetting(prisma, key, value) {
+  await prisma.Setting.upsert({
+    create: { settingKey: key, settingValue: value },
+    where: { settingKey: key },
+    update: {},
+  });
+}
+
+module.exports = {
+  exec: () => populateDatabase(),
+};
