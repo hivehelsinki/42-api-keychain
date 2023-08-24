daf352314502bfbb17aa092d4585ebfe9a70b85a
Titus <jgengo@student.42.fr>
2023-08-24 18:27:44 +0300

    fix(front): removed useless file + reduce ping size

--- front/src/components/ping.tsx
+++ front/src/components/ping.tsx
@@ -4,7 +4,7 @@ import { tv, type VariantProps } from 'tailwind-variants';
 import { cn } from '@/lib/utils';
 
 const pingVariants = tv({
-  base: 'rounded-full h-3 w-3',
+  base: 'rounded-full h-2 w-2',
   variants: {
     variant: {
       default: 'bg-green-400',
